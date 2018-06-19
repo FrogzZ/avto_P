c79c2ed77523b7ca19deef9037d562c5189d3bf2
FrogzZ <40285204+FrogzZ@users.noreply.github.com>
2018-06-20 02:03:59 +0300

    1

---
+++ Avto_poliv.ino
@@ -0,0 +1,55 @@
+
+  int bt1;
+  int motor = 10;
+  boolean i =0;
+  unsigned long nextTime = 0;
+  int led = 7;
+
+  
+void setup() {
+  pinMode(A5, INPUT_PULLUP);
+  Serial.begin(9600);
+  pinMode(motor, OUTPUT);
+  pinMode(led, OUTPUT);
+  
+  
+}
+
+void loop() {
+
+  if (nextTime <= millis()) 
+  {
+    nextTime = (millis() + (unsigned long)24*60*60*1000); // через сколько будет включаться
+    Serial.print(millis());
+    digitalWrite(motor, 1);
+    delay(5000); // сколько по времени будет работать
+  }
+  
+  boolean butA7 = !digitalRead(A5);
+  //Serial.println(butA7);
+  if (butA7 == 1) 
+  {
+    digitalWrite(10, 1);
+    delay(10);
+  }
+  
+  else  digitalWrite(10, 0);
+  
+
+  if (Serial.available())
+  {
+    bt1 = Serial.read();
+    
+    if (bt1 == '1')
+      {
+        digitalWrite(motor, HIGH);
+        delay(2000);
+      }
+      
+     if (bt1 == '0')
+      {
+        digitalWrite(motor, LOW);
+      }
+  }
+
+}
